f994d71bdf838b7443064e0517f06a6cf741484c
Koichi <koichisojiro@live.com>
2016-09-13 15:58:29 +0700

    added NotificationCompat

--- app/src/main/java/com/example/trannh08/ad012notifications/MainActivity.java
+++ app/src/main/java/com/example/trannh08/ad012notifications/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.trannh08.ad012notifications;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,11 +12,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String EXTRAS_NOTIFICATION_ID = "notificationID";
     private final int NOTIFICATION_ID_NOTIFICATION01 = 1;
     private final int NOTIFICATION_ID_NOTIFICATION02 = 2;
-    private final String EXTRA_MESSAGE = "messages";
+    private final String EXTRA_MESSAGE_01 = "messages_01";
+    private final String EXTRA_MESSAGE_02 = "messages_02";
     private Button button_notification01;
     private Button button_notification02;
+    private int numMessages = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Notify(NOTIFICATION_ID_NOTIFICATION01,
                         "You've received new notification.", "Notification 01",
-                        "This is the sample for Notification 01");
+                        EXTRA_MESSAGE_01, "This is the sample for Notification 01");
             }
         });
 
@@ -39,41 +42,66 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Notify(NOTIFICATION_ID_NOTIFICATION02,
                         "You've received new notification.", "Notification 02",
-                        "This is the sample for Notification 02");
+                        EXTRA_MESSAGE_02, "This is the sample for Notification 02");
             }
         });
     }
 
     @SuppressWarnings("deprecation")
     private void Notify(int notificationID, String notificationTitle, String notificationMessage,
-                        String extraMessage) {
+                        String extraMessageKey, String extraMessage) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         Intent intent = new Intent(this, NotificationActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, extraMessage);
+        intent.putExtra(extraMessageKey, extraMessage);
+        intent.putExtra(EXTRAS_NOTIFICATION_ID, notificationID);
 
-        // using PendingIntent.FLAG_UPDATE_CURRENT to pass parameter to PendingIntent
-        // default = 0
+        // using PendingIntent.FLAG_UPDATE_CURRENT to pass parameter to PendingIntent (default = 0)
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        // approach 01
-        Notification notification = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.ic_action_name)
-                .setContentTitle(notificationTitle)
-                .setContentText(notificationMessage)
-                .setContentIntent(pendingIntent)
-                .build();
-        // add this flag to make notification disappear after clicking
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        notificationManager.notify(notificationID, notification);
-
-        // approach 02
-//        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+        // approach 01: CLASSIC NOTIFICATION
+//        Notification notification = new Notification.Builder(this)
 //                .setSmallIcon(R.drawable.ic_action_name)
 //                .setContentTitle(notificationTitle)
-//                .setContentText(notificationMessage);
-//        builder.setContentIntent(pendingIntent);
-//        // set AutoCancel(true) to make notification disappear after clicking
-//        builder.setAutoCancel(true);
-//        notificationManager.notify(notificationID, builder.build());
+//                .setContentText(notificationMessage)
+//                .setContentIntent(pendingIntent)
+//                .build();
+//        // add this flag to make notification disappear after clicking
+//        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+//        notificationManager.notify(notificationID, notification);
+
+        // approach 02: MODERN NOTIFICATION
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_action_name)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationMessage);
+        builder.setContentIntent(pendingIntent);
+        // show number of notifications were showing
+        builder.setNumber(++numMessages);
+        // create an Inbox Style
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+        String[] events = new String[6];
+        events[0] = new String("This is 1st line....");
+        events[1] = new String("This is 2ne line...");
+        events[2] = new String("This is 3rd line...");
+        events[3] = new String("This is 4th line...");
+        events[4] = new String("This is 5th line...");
+        events[5] = new String("This is 6th line...");
+        // Sets a title for the Inbox style big view
+        inboxStyle.setBigContentTitle(notificationTitle + " details:");
+        // Moves events into the big view
+        for (int i=0; i < events.length; i++) {
+            inboxStyle.addLine(events[i]);
+        }
+        // tell program that NotificationCompat.Builder is using InboxStyle
+        builder.setStyle(inboxStyle);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(NotificationActivity.class);
+        /* Adds the Intent that starts the Activity to the top of the stack */
+        stackBuilder.addNextIntent(intent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, 0);
+        builder.setContentIntent(resultPendingIntent);
+        // set AutoCancel(true) to make notification disappear after clicking
+        builder.setAutoCancel(true);
+        notificationManager.notify(notificationID, builder.build());
     }
 }
--- app/src/main/java/com/example/trannh08/ad012notifications/NotificationActivity.java
+++ app/src/main/java/com/example/trannh08/ad012notifications/NotificationActivity.java
@@ -6,18 +6,30 @@ import android.widget.TextView;
 
 public class NotificationActivity extends AppCompatActivity {
 
-    private final String EXTRA_MESSAGE = "messages";
+    private final String EXTRAS_NOTIFICATION_ID = "notificationID";
+    private final String EXTRA_MESSAGE_01 = "messages_01";
+    private final String EXTRA_MESSAGE_02 = "messages_02";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
+        TextView textView = (TextView) findViewById(R.id.textView);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String value = extras.getString(EXTRA_MESSAGE);
-            TextView textView = (TextView) findViewById(R.id.textView);
-            textView.setText(value);
+            int value = extras.getInt(EXTRAS_NOTIFICATION_ID);
+            switch (value)
+            {
+                case 1:
+                    String str = extras.getString(EXTRA_MESSAGE_01);
+                    textView.setText(str);
+                    break;
+                case 2:
+                    String str2 = extras.getString(EXTRA_MESSAGE_02);
+                    textView.setText(str2);
+                    break;
+            }
         }
     }
 }
